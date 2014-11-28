8364f03bcb4fa86041fdd20dff13525677f5ceb7
Pedro Matos de Carvalho <pedromatosdecarvalho@gmail.com>
2014-11-28 21:58:38 +0000

    stream to create sql file created

--- server/controllers/base.js
+++ server/controllers/base.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 module.exports = {
 
     index: {
@@ -8,8 +10,30 @@ module.exports = {
 
     postScript: {
         handler: function(request, reply){
-            console.log("LOGGIN REQUEST " + request.payload)
-            reply(request.payload);
+            textify(request.payload.script, function(err, sqlFileStream){
+              if(err) throw err;
+              reply.pipe(sqlFileStream);
+            });
+
+            function textify(script, callback){
+              if(script == null){
+                var no_script_error = new Error('Script is null');
+                no_script_error.null_script = true;
+                callback(no_script_error, null);
+              }
+              var ws = fs.createWriteStream(script.date + '_' + script.name + '.sql');
+              if(script.objective || script.target || script.procedure || script.expectedResult){
+                  if(script.objective) ws.write('/*\n\tObjectivo: ' + script.objective + '\n\t');
+                  if(script.objective) ws.write('\tAlvo: ' + script.target + '\n\t');
+                  if(script.objective) ws.write('\tProdecimento: ' + script.procedure + '\n\t');
+                  if(script.objective) ws.write('\tResultado Esperado: ' + script.expectedResult + '\n*/\n');
+              }
+              ws.write('SPOOL G:\\AGOC-NP\\NP\\Operacao\\logs\\' + script.schema+ '\\' + script.date + '\\' + script.date + '_' + script.name + '.log\n');
+              ws.write('\nset echo on;\n');
+              ws.write(script.query);
+              ws.write('\ncommit;\n\nset echo off;\n\nSPOOL OFF');
+              callback(null, ws);
+            };
         }
     },
 
