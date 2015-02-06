d7321851fb6273123bbbe3581382d8d15dad57b0
Pedro Carvalho <pcarvalho@opensoft.pt>
2015-02-06 17:49:19 +0000

    client only blob test

--- public/scripts/controllers/ScriptDataCtrl.js
+++ public/scripts/controllers/ScriptDataCtrl.js
@@ -11,9 +11,29 @@ sqlScriptGenerator.controller('ScriptDataCtrl', ['$scope', '$filter','ScriptData
 	$scope.submitForm = function(isValid, script) {
 		// check to make sure the form is completely valid
     if (isValid) {
-      ScriptDataFactory.postScript(script).success(function(){
-        window.location = '/scripts/' + $filter('date')(script.date, 'yyyyMMdd') + '_' + script.name + '.sql';
-      });
+
+
+      var blob = new Blob(["SPOOL G:\\AGOC-NP\\NP\\Operacao\\logs\\",
+        $scope.script.env,
+        "\\",
+        $filter('date', 'yyyyMMdd')($scope.script.date),
+        "\\",
+        $filter('date', 'yyyyMMdd')($scope.script.date),
+        "_",
+        $scope.script.name,
+        ".log\n\nSET ECHO ON;\n\nALTER SESSION SET current_schema=",
+        $scope.script.schema,
+        ";\n\nALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS';\n\n",
+        $filter('filt', $scope.script.commit)($scope.script.query),
+        "\n\nCOMMIT;\n\nSET ECHO OFF;\n\nSPOOL OFF"
+
+      ], {type: "text/plain;charset=utf-8"});
+
+      saveAs(blob, $filter('date', 'yyyyMMdd')($scope.script.date) + '_' + $scope.script.name + '.log');
+
+      //ScriptDataFactory.postScript(script).success(function(){
+      //  window.location = '/scripts/' + $filter('date')(script.date, 'yyyyMMdd') + '_' + script.name + '.sql';
+      //});
 		}
 		else {
 			alert('Form is not valid!');
