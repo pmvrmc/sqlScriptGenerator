332bdb1eac72af1e5652ae826d769f6531d697d9
Pedro Matos de Carvalho <pedromatosdecarvalho@gmail.com>
2015-02-08 14:59:25 +0000

    fileSaver working on client, no need for server now

--- public/scripts/controllers/ScriptDataCtrl.js
+++ public/scripts/controllers/ScriptDataCtrl.js
@@ -1,12 +1,15 @@
 'use strict';
 
-sqlScriptGenerator.controller('ScriptDataCtrl', ['$scope', '$filter','ScriptDataFactory', function($scope, $filter, ScriptDataFactory) {
+sqlScriptGenerator.controller('ScriptDataCtrl', ['$scope', '$filter', function($scope, $filter) {
+
+  $scope.encodings = ['UTF-8', 'WINDOWS-1252'];
 
   //the object that will store the form data aka ng-model
   $scope.script = {};
   $scope.script.commit = 0;
   $scope.script.query = '';
   $scope.script.date = new Date();
+  $scope.script.encoding = $scope.encodings[0];
 
 	$scope.submitForm = function(isValid, script) {
 		// check to make sure the form is completely valid
@@ -16,24 +19,21 @@ sqlScriptGenerator.controller('ScriptDataCtrl', ['$scope', '$filter','ScriptData
       var blob = new Blob(["SPOOL G:\\AGOC-NP\\NP\\Operacao\\logs\\",
         $scope.script.env,
         "\\",
-        $filter('date', 'yyyyMMdd')($scope.script.date),
+        $filter('date')($scope.script.date, 'yyyyMMdd'),
         "\\",
-        $filter('date', 'yyyyMMdd')($scope.script.date),
+        $filter('date')($scope.script.date, 'yyyyMMdd'),
         "_",
         $scope.script.name,
         ".log\n\nSET ECHO ON;\n\nALTER SESSION SET current_schema=",
         $scope.script.schema,
         ";\n\nALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD HH24:MI:SS';\n\n",
-        $filter('filt', $scope.script.commit)($scope.script.query),
+        $filter('addCommits')($scope.script.query, $scope.script.commit),
         "\n\nCOMMIT;\n\nSET ECHO OFF;\n\nSPOOL OFF"
 
-      ], {type: "text/plain;charset=utf-8"});
+      ], {type: "text/plain;charset=" + $scope.script.encoding });
 
-      saveAs(blob, $filter('date', 'yyyyMMdd')($scope.script.date) + '_' + $scope.script.name + '.log');
+      saveAs(blob, $filter('date')($scope.script.date, 'yyyyMMdd') + '_' + $scope.script.name + '.sql');
 
-      //ScriptDataFactory.postScript(script).success(function(){
-      //  window.location = '/scripts/' + $filter('date')(script.date, 'yyyyMMdd') + '_' + script.name + '.sql';
-      //});
 		}
 		else {
 			alert('Form is not valid!');
--- public/scripts/controllers/ScriptDataFilter.js
+++ public/scripts/controllers/ScriptDataFilter.js
@@ -1,6 +1,6 @@
 'use strict';
 
-sqlScriptGenerator.filter('filt', function(_){
+sqlScriptGenerator.filter('addCommitsPreview', function(_){
 
   return function(query, nLines){
     //eliminate empty lines
@@ -27,5 +27,35 @@ sqlScriptGenerator.filter('filt', function(_){
     }
     return result;
   }
+
+});
+
+sqlScriptGenerator.filter('addCommits', function(_){
+
+  return function(query, nLines){
+    //eliminate empty lines
+    var str = query.split('\n');
+    str = _.filter(str, function(line){return line.trim().length;});
+
+    if(nLines === 0 ) return str.join('\n');
+
+    var result = '';
+    var linesToCommit = nLines;
+
+    for(var i = 0; i < str.length; i++){
+
+      //if last char was ';' and we have already passed nLines, COMMIT!
+      if( (str[i].indexOf(';', str[i].length - ';'.length) !== -1)
+        && (linesToCommit <= 1) ){
+          result = result.concat('\n' + str[i] + '\n\nCOMMIT;\n');
+          linesToCommit = nLines;
+        }
+        else {
+          result = result.concat('\n' + str[i]);
+          --linesToCommit;
+        }
+      }
+      return result;
+    }
 
 });
